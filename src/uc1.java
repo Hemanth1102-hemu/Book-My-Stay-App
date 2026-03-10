ad827bef3ace0700513fb141871d8d480f15f1ff
ArushiJ0 <arushi.123253@gmail.com>
2026-03-10 14:31:54 +0530

    Added UC1 - Book My Stay App

---
+++ src/uc1.java
@@ -0,0 +1,18 @@
+/**
+ * Book My Stay - Hotel Booking Management System
+ * Entry point of the application.
+ *
+ * @author Hemanth
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Application: Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("Thank you for using Book My Stay!");
+
+    }
+}
