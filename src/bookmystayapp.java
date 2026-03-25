76c5cbe5a0f7aa40bc8006e23ad17acd907f848b
your GitHub username <hemanthdevarasetty@gmail.com>
2026-03-25 15:55:38 +0530

    Added8- Booking History & Reporting

--- src/bookmystayapp.java
+++ src/bookmystayapp.java
@@ -1,112 +1,102 @@
 import java.util.*;
 
-public class UseCase6RoomAllocationService {
-     private Queue<String> bookingRequestQueue = new LinkedList<>();
+class Reservation {
+    private String guestName;
+    private String roomType;
+    private String roomID;
 
-    private Map<String, Integer> roomInventory = new HashMap<>();
+    public Reservation(String guestName, String roomType, String roomID) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.roomID = roomID;
+    }
 
-    private Set<String> allocatedRoomIDs = new HashSet<>();
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public String getRoomID() {
+        return roomID;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest: " + guestName + ", Room Type: " + roomType + ", Room ID: " + roomID;
+    }
+}
+
+public class UseCase8BookingHistoryReport {
 
 
-    private Map<String, Set<String>> roomTypeAllocations = new HashMap<>();
+    private List<Reservation> bookingHistory = new ArrayList<>();
 
-    public UseCase6RoomAllocationService() {
-        roomInventory.put("Deluxe", 3);
-        roomInventory.put("Suite", 2);
-        roomInventory.put("Standard", 4);
 
-        roomTypeAllocations.put("Deluxe", new HashSet<>());
-        roomTypeAllocations.put("Suite", new HashSet<>());
-        roomTypeAllocations.put("Standard", new HashSet<>());
+
+    public void addReservationToHistory(String guestName, String roomType, String roomID) {
+        Reservation reservation = new Reservation(guestName, roomType, roomID);
+        bookingHistory.add(reservation);
+
+        System.out.println("✔ Added to Booking History → " + reservation);
     }
 
 
 
-    public void addBookingRequest(String roomType) {
-        System.out.println("Booking Request Added: " + roomType);
-        bookingRequestQueue.add(roomType);
-    }
-
-
-
-    public void processReservations() {
-        while (!bookingRequestQueue.isEmpty()) {
-
-            String requestedType = bookingRequestQueue.poll();  // FIFO
-            System.out.println("\nProcessing booking request for room type: " + requestedType);
-
-
-            if (!roomInventory.containsKey(requestedType)) {
-                System.out.println("Invalid room type. Cannot allocate.");
-                continue;
-            }
-
-            if (roomInventory.get(requestedType) <= 0) {
-                System.out.println("No rooms available for type: " + requestedType);
-                continue;
-            }
-
-
-            String assignedRoomID = generateUniqueRoomID(requestedType);
-
-
-            if (assignedRoomID == null) {
-                System.out.println("Unable to assign a unique room ID!");
-                continue;
-            }
-
-
-            allocatedRoomIDs.add(assignedRoomID);
-            roomTypeAllocations.get(requestedType).add(assignedRoomID);
-
-
-            roomInventory.put(requestedType, roomInventory.get(requestedType) - 1);
-
-            System.out.println("✅ Reservation Confirmed!");
-            System.out.println("Assigned Room ID: " + assignedRoomID);
-            System.out.println("Remaining " + requestedType + " rooms: " + roomInventory.get(requestedType));
-        }
-    }
-    private String generateUniqueRoomID(String roomType) {
-        while (true) {
-            String id = roomType.substring(0, 2).toUpperCase() + "-" + (int)(Math.random() * 9000 + 1000);
-
-            if (!allocatedRoomIDs.contains(id)) {
-                return id;
-            }
-        }
-    }
-
-
-
-    public void showAllocationReport() {
+    public void showCompleteBookingHistory() {
         System.out.println("\n==============================");
-        System.out.println(" FINAL ROOM ALLOCATION REPORT");
+        System.out.println("     COMPLETE BOOKING HISTORY ");
         System.out.println("==============================");
 
-        for (String type : roomTypeAllocations.keySet()) {
-            System.out.println("Room Type: " + type);
-            System.out.println("Allocated Rooms: " + roomTypeAllocations.get(type));
-            System.out.println("Remaining Inventory: " + roomInventory.get(type));
-            System.out.println();
+        if (bookingHistory.isEmpty()) {
+            System.out.println("No bookings found!");
+            return;
+        }
+
+        for (Reservation r : bookingHistory) {
+            System.out.println(r);
         }
     }
 
+
+
+    public void generateSummaryReport() {
+        System.out.println("\n==============================");
+        System.out.println("        SUMMARY REPORT");
+        System.out.println("==============================");
+
+        if (bookingHistory.isEmpty()) {
+            System.out.println("No data available for summary.");
+            return;
+        }
+
+        Map<String, Integer> roomTypeCount = new HashMap<>();
+
+        for (Reservation r : bookingHistory) {
+            roomTypeCount.put(r.getRoomType(),
+                    roomTypeCount.getOrDefault(r.getRoomType(), 0) + 1);
+        }
+
+        for (String roomType : roomTypeCount.keySet()) {
+            System.out.println(roomType + " Rooms Booked: " + roomTypeCount.get(roomType));
+        }
+    }
 
 
     public static void main(String[] args) {
 
-        UseCase6RoomAllocationService service = new UseCase6RoomAllocationService();
+        UseCase8BookingHistoryReport reportSystem = new UseCase8BookingHistoryReport();
 
 
-        service.addBookingRequest("Deluxe");
-        service.addBookingRequest("Suite");
-        service.addBookingRequest("Suite");
-        service.addBookingRequest("Deluxe");
-        service.addBookingRequest("Standard");
-        service.addBookingRequest("Deluxe");
-        service.addBookingRequest("Suite");
-        service.processReservations();
-        service.showAllocationReport();
+        reportSystem.addReservationToHistory("Arjun", "Deluxe", "DE-1023");
+        reportSystem.addReservationToHistory("Priya", "Suite", "SU-8764");
+        reportSystem.addReservationToHistory("Rahul", "Standard", "ST-2211");
+        reportSystem.addReservationToHistory("Meera", "Deluxe", "DE-3421");
+
+                reportSystem.showCompleteBookingHistory();
+
+               reportSystem.generateSummaryReport();
     }
 }
